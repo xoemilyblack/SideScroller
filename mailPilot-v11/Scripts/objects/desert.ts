e6f5e1ea4db6cfcf0f9c88ef16ec282f690dae55
xoemilyblack <xoemilyblack@hotmail.co.uk>
2014-11-07 12:11:15 -0500

    Initial Start - with base code and new background

---
+++ mailPilot-v11/Scripts/objects/desert.ts
@@ -0,0 +1,40 @@
+﻿/// <reference path="../managers/asset.ts" />
+module objects {
+    // Ocean Class
+    export class Desert {
+        image: createjs.Bitmap;
+        stage: createjs.Stage;
+        game: createjs.Container;
+        width: number;
+        height: number;
+        dx: number;
+        constructor(stage: createjs.Stage, game: createjs.Container) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Bitmap(managers.Assets.loader.getResult("desert"));
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+            this.reset();
+
+            this.dx = -5;
+
+            game.addChild(this.image);
+        }
+
+        update() {
+            this.image.x += this.dx;
+            if (this.image.x <= 0) {
+                this.reset();
+            }
+        }
+
+        reset() {
+            this.image.x = -960;
+        }
+
+        destroy() {
+            game.removeChild(this.image);
+        }
+    }
+
+}
---
+++ mailPilot-v11/Scripts/objects/ocean.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../managers/asset.ts" />
+var objects;
+(function (objects) {
+    // Ocean Class
+    var Desert = (function () {
+        function Desert(stage, game) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Bitmap(managers.Assets.loader.getResult("desert"));
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+            this.reset();
+
+            this.dx = -5;
+
+            game.addChild(this.image);
+        }
+        Desert.prototype.update = function () {
+            this.image.x += this.dx;
+            if (this.image.x >= 0) {
+                this.reset();
+            }
+        };
+
+        Desert.prototype.reset = function () {
+            this.image.x = -960;
+        };
+
+        Desert.prototype.destroy = function () {
+            game.removeChild(this.image);
+        };
+        return Desert;
+    })();
+    objects.Desert = Desert;
+})(objects || (objects = {}));
+//# sourceMappingURL=ocean.js.map
